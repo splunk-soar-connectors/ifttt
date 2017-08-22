0fddf1c95f23ccd8be8e2d195e684d02e5332eed
Ryan Means <>
2017-08-21 18:58:12 -0700

    Add initial ifttt code

---
+++ ifttt_consts.py
@@ -0,0 +1,13 @@
+IFTTT_JSON_APIKEY = "api_key"
+
+IFTTT_ERR_API_TEST = "API Key is not valid"
+IFTTT_SUCC_API_TEST = "API Key test successful"
+IFTTT_ERR_RESPONSE_IS_NOT_JSON = "Response is not a valid JSON"
+IFTTT_ERR_TRIGGER = "Trigger Failed.  Check API key or Network Connection"
+IFTTT_ERR_SERVER_CONNECTION = "Validate Internet Connectivity / Proxy Config"
+
+IFTTT_MAKER_BASE_URL = "https://maker.ifttt.com/trigger/{0}/with/key/{1}"
+
+ACTION_ID_TRIGGER_ACTION = "trigger_ifttt_action"
+ACTION_ID_SEND_MESSAGE = "send_sms_message"
+ACTION_ID_SAVE_TO_DRIVE = "save_to_google_drive"
